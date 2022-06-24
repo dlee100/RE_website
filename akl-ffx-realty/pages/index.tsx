281a14bb97464cbf1f41b93ba88917a3cef572a2
dlee100 <danieljplee1@gmail.com>
2022-06-24 00:39:12 -0400

    Cleaning up page and adding gitignore

--- akl-ffx-realty/pages/index.tsx
+++ akl-ffx-realty/pages/index.tsx
@@ -14,52 +14,16 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to the start of my mom's realty website!
-        </h1>
-
-        <p className={styles.description}>
-          The design and development will be by Nick Baek and Daniel Lee of {' '}
-          <code className={styles.code}>PRSNA Studio LLC</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
           <code className={styles.canary}>This Is Your New Home.</code>
         </h1>
 
         <p className={styles.description}>
           Contact Kay Agnes Lee of Fairfax Realty!
+        </p>
+
+        <p className={styles.description}>
+          The design and development will be by Nick Baek and Daniel Lee of {' '}
+          <code className={styles.code}>PRSNA Studio LLC</code>
         </p>
 
         <div className={styles.grid}>
@@ -99,7 +63,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Designed, developed, and maintained by{' '}
+          Designed, Developed, and Maintained by{' '}
           <span className={styles.logo}>
             <Image src="/PRSNA_Logo_Dark_Grey.svg" alt="PRSNA_studio Logo" width={72} height={16} />
           </span>
